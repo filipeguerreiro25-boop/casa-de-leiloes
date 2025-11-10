e3a425908be6d750138a910781ac3da021dd9f7f
Filipe Guerreiro <filipeguerreiro25@gmail.com>
2025-11-10 11:56:10 -0300

    Adicionada mensagem de sucesso/erro no cadastro e definida CadastroVIEW como tela principal

--- src/cadastroVIEW.java
+++ src/cadastroVIEW.java
@@ -140,17 +140,30 @@ public class cadastroVIEW extends javax.swing.JFrame {
     }//GEN-LAST:event_cadastroNomeActionPerformed
 
     private void btnCadastrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCadastrarActionPerformed
+         try {
         ProdutosDTO produto = new ProdutosDTO();
         String nome = cadastroNome.getText();
         String valor = cadastroValor.getText();
         String status = "A Venda";
+
         produto.setNome(nome);
         produto.setValor(Integer.parseInt(valor));
         produto.setStatus(status);
-        
+
         ProdutosDAO produtodao = new ProdutosDAO();
         produtodao.cadastrarProduto(produto);
-        
+
+        // Mensagem de sucesso
+        javax.swing.JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
+
+        // Limpar os campos após salvar
+        cadastroNome.setText("");
+        cadastroValor.setText("");
+
+    } catch (Exception e) {
+        // Mensagem de erro
+        javax.swing.JOptionPane.showMessageDialog(null, "Erro ao cadastrar produto: " + e.getMessage());
+    }
     }//GEN-LAST:event_btnCadastrarActionPerformed
 
     private void btnProdutosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnProdutosActionPerformed
